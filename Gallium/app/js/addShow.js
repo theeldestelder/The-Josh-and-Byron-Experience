bdd364ef235cbc50716c35985e966fdb5b1a37a3
Byron Ambright <bambright@ptc.com>
2018-09-23 16:12:01 -0500

    Add table functionality for shows.html

--- Gallium/app/js/addShow.js
+++ Gallium/app/js/addShow.js
@@ -4,7 +4,7 @@ var path = parent.require('path');
 var win = parent.require('electron').remote.BrowserWindow;
 
 function popWindow() {
-    var popup = new win({width: 600, height: 400});
+    var popup = new win({ width: 600, height: 400 });
     popup.loadFile('./app/html/addShow.html');
 }
 
@@ -19,17 +19,8 @@ function addNewShow() {
         fs.mkdirSync(showPath);
         alert("New show " + inputText + " added");
         fs.copyFileSync(emptyShowPath, path.join(showPath, 'show.json'));
-
-
-        var appConfigPath = path.join(parent.__dirname, './config/shows.json');
-        var appConfig = JSON.parse(fs.readFileSync(appConfigPath));
-
-        var activeShow = appConfig.activeShow;
-        var shows = appConfig.shows;
-
-        addShowToConfig(appConfigPath);
-    } 
-
+        addShowToConfig(showPath);
+    }
     closePopupWindow();
 }
 
@@ -37,22 +28,14 @@ function closePopupWindow() {
     parent.require('electron').remote.getCurrentWindow().close();
 }
 
-function addShowToConfig(jsonPath) {
-    var data = JSON.parse(fs.readFileSync(jsonFiles[i]));
-
+function addShowToConfig(showPath) {
+    var appConfigPath = path.join(parent.__dirname, '../config/shows.json');
+    var data = JSON.parse(fs.readFileSync(appConfigPath));
     if (data) {
-        var experiences = data.experiences;
-            for (z in experiences) {
-                if (typeof experiences[z].thingCode !== 'undefined') {
-                    experiences[z].thingCode = newData;
-                } else if (typeof experiences[z].vumark !== 'undefined') {
-                    experiences[z].vumark = newData;
-                }
-                if (typeof experiences[z]['index-keys'] !== 'undefined' && experiences[z]['index-keys'] !== []) {
-                    experiences[z]['index-keys'] = ['urn:vuforia:vumark:' + newData];
-                }
-            }
-            fs.writeFileSync(jsonFiles[i], JSON.stringify(data, null, 2));
-        }
+        var shows = data.shows;
+    
+        shows.push(showPath);
+        fs.writeFileSync(appConfigPath, JSON.stringify(data, null, 2));
+        
     }
 }
